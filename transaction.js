4196636c76da50c448bd056aeb9f917a427829f0
ramishahq <hoque2@usf.edu>
2024-11-10 11:58:48 -0500

    // commit Create transaction module with Transaction class.

---
+++ transaction.js
@@ -0,0 +1,38 @@
+
+
+import { getAssetById } from './asset.js';
+
+export class Transaction {
+    constructor(assetId, type, quantity) {
+        const asset = getAssetById(assetId);
+
+        if (!asset) {
+            throw new Error('Sorry, asset not found');
+        }
+
+        this.assetId = assetId;
+        this.type = type;
+        this.quantity = quantity;
+        this.assetName = asset.name;
+        this.pricePerUnit = asset.price;
+
+        // Update the asset quantity 
+        if (type === 'buy') {
+            asset.quantity += quantity;
+        } else if (type === 'sell') {
+            if (asset.quantity < quantity) {
+                throw new Error(`Sorry, insufficient quantity for sale of ${asset.name}`);
+            }
+            asset.quantity -= quantity;
+        } else {
+            throw new Error('Invalid transaction type.');
+        }
+    }
+
+
+    getReceipt() {
+        const action = this.type === 'buy' ? 'Bought' : 'Sold';
+        const totalValue = this.pricePerUnit * this.quantity;
+        return `${action} ${this.quantity} of ${this.assetName} at $${this.pricePerUnit} each. Total: $${totalValue.toFixed(2)}`;
+    }
+}
